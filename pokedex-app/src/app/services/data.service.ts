d2d1b1048e46ec7f7ea37269063622580a6a388e
Matheus <matheusrdeoliv1@gmail.com>
2021-08-30 14:53:04 -0300

    implementando paginação

--- pokedex-app/src/app/services/data.service.ts
+++ pokedex-app/src/app/services/data.service.ts
@@ -10,8 +10,8 @@ export class DataService {
     private http: HttpClient
   ) { }
 
-  getPokemons() {
-    return this.http.get(`https://pokeapi.co/api/v2/pokemon?limit=12`)
+  getPokemons(limit: number, offset: number) {
+    return this.http.get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
   }
 
   getMoreData(name: string) {
